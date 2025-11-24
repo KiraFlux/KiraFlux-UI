52c52241553353fb64f9f959c432a7946ef359c3
KiraFlux <pret2titio1n@gmail.com>
2025-11-24 13:21:38 +0300

    Extract UI::Render from UI

---
+++ src/kf/ui/Render.hpp
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+#include <kf/aliases.hpp>
+
+namespace kf::ui {
+
+/// @brief Система отрисовки
+struct Render {
+
+private:
+    std::array<u8, 128> buffer{};
+    usize cursor{0};
+
+public:
+    u16 rows{8}, cols{21};
+
+    /// @brief Подготовить буфер
+    slice<const u8> data() {
+        buffer[cursor] = '\0';
+
+        return {
+            buffer.data(),
+            cursor,
+        };
+    }
+
+    /// @brief Сбросить буфер отрисовки
+    void reset() {
+        cursor = 0;
+    }
+
+    // Значения
+
+    void string(const char *str) {
+        (void) print(str);
+    }
+
+    void number(i32 integer) {
+        (void) print(integer);
+    }
+
+    void number(f64 real, u8 rounding) {
+        (void) print(real, rounding);
+    }
+
+    // Оформление
+
+    void arrow() {
+        (void) write('-');
+        (void) write('>');
+        (void) write(' ');
+    }
+
+    void colon() {
+        (void) write(':');
+        (void) write(' ');
+    }
+
+    void contrastBegin() {
+        (void) write(0x81);
+    }
+
+    void contrastEnd() {
+        (void) write(0x80);
+    }
+
+    void blockBegin() {
+        (void) write('[');
+    }
+
+    void blockEnd() {
+        (void) write(']');
+    }
+
+    void variableBegin() {
+        (void) write('<');
+    }
+
+    void variableEnd() {
+        (void) write('>');
+    }
+
+    // Управление
+
+    void widgetEnd() {
+        (void) write('\n');
+    }
+
+private:
+    [[nodiscard]] usize print(const char *str) {
+        if (nullptr == str) {
+            str = "nullptr";
+        }
+
+        usize written{0};
+
+        while (*str) {
+            written += write(*str);
+            str += 1;
+        }
+
+        return written;
+    }
+
+    [[nodiscard]] usize print(i32 integer) {
+        usize written{0};
+
+        if (integer < 0) {
+            integer = -integer;
+            written += write('-');
+        }
+
+        char digits_buffer[12];
+
+        auto digits_total{0};
+        while (integer > 0) {
+            const auto base = 10;
+
+            digits_buffer[digits_total] = static_cast<char>(integer % base + '0');
+            digits_total += 1;
+            integer /= base;
+        }
+
+        for (auto i = digits_total - 1; i >= 0; i -= 1) {
+            written += write(digits_buffer[i]);
+        }
+
+        return written;
+    }
+
+    [[nodiscard]] usize print(f64 real, u8 rounding) {
+        if (std::isnan(real)) {
+            return print("nan");
+        }
+
+        if (std::isinf(real)) {
+            return print("inf");
+        }
+
+        usize written{0};
+
+        if (real < 0) {
+            real = -real;
+            written += write('-');
+        }
+
+        written += print(i32(real));
+
+        if (rounding > 0) {
+            written += write('.');
+
+            auto fractional = real - i32(real);
+
+            for (auto i = 0; i < rounding; i += 1) {
+                const auto base = 10;
+
+                fractional *= base;
+                const auto digit = u8(fractional);
+                written += write('0' + digit);
+                fractional -= digit;
+            }
+        }
+
+        return written;
+    }
+
+    /// @brief Записать байт в буфер
+    [[nodiscard]] usize write(u8 c) {
+        if (cursor >= buffer.size()) {
+            return 0;
+        }
+
+        buffer[cursor] = c;
+        cursor += 1;
+        return 1;
+    }
+};
+
+}// namespace kf::ui
