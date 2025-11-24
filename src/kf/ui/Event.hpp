134e623145a946ed95d3f8b17883eb5857208a1b
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-11-24 23:22:58 +0300

    update Event.hpp

--- src/kf/ui/Event.hpp
+++ src/kf/ui/Event.hpp
@@ -1,64 +1,78 @@
 #pragma once
 
-static constexpr auto counter_start = __COUNTER__ + 1;
-
-#define enumerate() ((__COUNTER__ - counter_start) << value_bits)
+#define enumerate(x) ((x) << value_bits)
 
 namespace kf::ui {
 
 /// @brief Входящее событие
 struct Event {
 
-public:
-    using Value = u8;
-
 private:
+    using Storage = u8;
 
-    static constexpr unsigned event_bits_total = sizeof(Value) * 8;
-    static constexpr Value event_value_full = (1 << event_bits_total) - 1;
+    static constexpr unsigned event_bits_total = sizeof(Storage) * 8;
+    static constexpr Storage event_value_full = (1 << event_bits_total) - 1;
 
     static constexpr unsigned type_bits = 3;
     static constexpr unsigned value_bits = event_bits_total - type_bits;
 
-    static constexpr Value value_mask = (1 << value_bits) - 1;
-    static constexpr Value type_mask = event_value_full & ~value_mask;
+    static constexpr Storage value_mask = (1 << value_bits) - 1;
+    static constexpr Storage type_mask = event_value_full & ~value_mask;
 
-    Value e;
+    Storage storage;
+
 public:
-
     /// @brief Тип события
-    enum class Type : Value {
+    enum class Type : Storage {
         // Управление
 
         /// @brief Ничего (Заглушка)
-        None = enumerate(),
+        None = enumerate(0),
 
         /// @brief Принудительный рендер
-        Update = enumerate(),
+        Update = enumerate(1),
 
         // События страницы
 
         /// @brief Смещение курсора
-        PageCursorMove = enumerate(),
+        /// @details Может содержать value
+        PageCursorMove = enumerate(2),
 
         // События виджета
 
         /// @brief Клик
-        WidgetClick = enumerate(),
+        WidgetClick = enumerate(3),
 
         /// @brief Получено значение
-        WidgetValueChange = enumerate(),
+        /// @details Может содержать value
+        WidgetValueChange = enumerate(4),
     };
 
-    constexpr explicit Event(Type type, Value value = 0) :
-        e{static_cast<Value>((static_cast<Value>(type) & type_mask) | (value & value_mask))} {}
+    /// @brief Примитив значения
+    using Value = i8;
 
-    [[nodiscard]] inline Type type() const {
-        return static_cast<Type>(e & type_mask);
+    /// @param type Тип события
+    /// @param value Значение
+    constexpr explicit Event(Type type, Value value = 0) :
+        storage{
+            static_cast<Storage>(
+                (static_cast<Storage>(type) & type_mask) |
+                (static_cast<Storage>(value) & value_mask)
+            )
+        } {}
+
+    [[nodiscard]] constexpr Type type() const {
+        return static_cast<Type>(storage & type_mask);
     }
 
-    [[nodiscard]] inline Value value() const {
-        return static_cast<Value>(e & value_mask);
+    [[nodiscard]] constexpr Value value() const {
+        constexpr auto sign_bit_mask = 1 << (value_bits - 1);
+
+        const auto result = static_cast<Value>(storage & value_mask);
+
+        return (result & sign_bit_mask) ?
+               static_cast<Value>(result | ~value_mask) :
+               result;
     }
 };
 
